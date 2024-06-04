ef2a3ee1ea884ce5c926168e8c767abbdca6114f
Shashant Kumar <shashant.sah@gmail.com>
2024-06-04 13:47:35 +0530

    hello.cpp created

---
+++ hello.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+#define cy cout<<"YES\n"
+#define cn cout<<"NO\n"
+#define int long long
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+ const int M=1e9+7;
+ void solve(){
+   int n;cin>>n;vi v(n+1);for(int i=1;i<=n;i++){cin>>v[i];}
+         
+         
+ }
+ signed main(){
+     fast
+     int cases=1;
+     cin>>cases;
+     while(cases--){
+         solve();
+     }
+     return 0;
+ }
